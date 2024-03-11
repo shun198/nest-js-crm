2ed1f4fce1481781964b608e35c3d5eebfaa732b
s.hirose <shunichi_hirose@pj100.biz>
2024-03-11 09:10:10 +0900

    👍 edit list

--- application/src/user/user.service.ts
+++ application/src/user/user.service.ts
@@ -7,13 +7,31 @@ export class UserService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async findAll() {
-    return this.prismaService.user.findMany();
+    const users = await this.prismaService.user.findMany({
+      include: {
+        groups: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
+    return users.map((user) => ({
+      id: user.id,
+      name: user.name,
+      employee_number: user.employee_number,
+      email: user.email,
+      is_active: user.is_active,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+      groups: user.groups.map((group) => group.name).join(', '),
+    }));
   }
 
   async findOne(id: number) {
     const user = await this.prismaService.user.findUnique({ where: { id } });
     if (!user) {
-      throw new NotFoundException(`User with ID ${id} not found`);
+      throw new NotFoundException(`ID:${id}を持つユーザは存在しません`);
     }
     return user;
   }
@@ -27,7 +45,7 @@ export class UserService {
       where: { id },
     });
     if (!existingUser) {
-      throw new NotFoundException(`User with ID ${id} not found`);
+      throw new NotFoundException(`ID:${id}を持つユーザは存在しません`);
     }
     return this.prismaService.user.update({
       where: { id },
@@ -40,7 +58,7 @@ export class UserService {
       where: { id },
     });
     if (!existingUser) {
-      throw new NotFoundException(`User with ID ${id} not found`);
+      throw new NotFoundException(`ID:${id}を持つユーザは存在しません`);
     }
     return this.prismaService.user.delete({ where: { id } });
   }
