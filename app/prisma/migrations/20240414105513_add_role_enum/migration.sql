2e7223a22676d0f81857e41ac14a5697785b3889
s.hirose <shunichi_hirose@pj100.biz>
2024-04-16 13:55:41 +0900

    👍 migrationファイルを更新

--- app/prisma/migrations/20240414105513_add_role_enum/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "Role" AS ENUM ('ADMIN', 'GENERAL');
-
--- AlterTable
-ALTER TABLE "Invitation" ADD COLUMN     "role" "Role" NOT NULL DEFAULT 'ADMIN';
--- app/prisma/migrations/20240416045434_init/migration.sql
+++ app/prisma/migrations/20240416045434_init/migration.sql
@@ -1,9 +1,30 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'GENERAL');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "employee_number" VARCHAR(8) NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'ADMIN',
+    "is_active" BOOLEAN NOT NULL DEFAULT true,
+    "is_verified" BOOLEAN NOT NULL DEFAULT false,
+    "is_superuser" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateTable
 CREATE TABLE "Invitation" (
     "id" SERIAL NOT NULL,
     "token" VARCHAR(255) NOT NULL,
     "is_used" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "expiry" TIMESTAMP(3) NOT NULL,
     "userId" INTEGER NOT NULL,
 
     CONSTRAINT "Invitation_pkey" PRIMARY KEY ("id")
@@ -15,10 +36,17 @@ CREATE TABLE "PasswordReset" (
     "token" VARCHAR(255) NOT NULL,
     "is_used" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "expiry" TIMESTAMP(3) NOT NULL,
     "userId" INTEGER NOT NULL,
 
     CONSTRAINT "PasswordReset_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_employee_number_key" ON "User"("employee_number");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- AddForeignKey
 ALTER TABLE "Invitation" ADD CONSTRAINT "Invitation_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
