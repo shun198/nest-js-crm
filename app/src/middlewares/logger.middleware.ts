f1fbfaabe068b7236594f2b5d9159684eb6c760b
s.hirose <shunichi_hirose@pj100.biz>
2024-04-16 14:33:15 +0900

    ♻️ update log

--- app/src/middlewares/logger.middleware.ts
+++ app/src/middlewares/logger.middleware.ts
@@ -6,11 +6,17 @@ export class LoggerMiddleware implements NestMiddleware {
   private readonly logger = new Logger('HTTP');
 
   use(request: Request, response: Response, next: NextFunction): void {
-    const { ip, method, originalUrl } = request;
+    const { ip, method, originalUrl, session } = request;
+    // ログレベル、ハンドラー名、時刻、ログIDを追加したい
     response.on('finish', () => {
       const { statusCode } = response;
-      // ログレベル、ハンドラー名、時刻、ログIDを追加したい
-      this.logger.log(`${ip} ${method} ${originalUrl} ${statusCode}`);
+      if (session['user']) {
+        this.logger.log(
+          `${ip} ${method} 社員番号:${session['user'].employee_number} ${originalUrl} ${statusCode}`,
+        );
+      } else {
+        this.logger.log(`${ip} ${method} ${originalUrl} ${statusCode}`);
+      }
     });
     next();
   }
